5c50345bb01efffbb408bb6699fa9311b40b2516
bob walker <bob@randomness.org.uk>
2013-10-19 14:58:33 -0400

    You can only have one --modulepath.

--- apply.sh
+++ apply.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-puppet apply --hiera_config=hiera.yaml --modulepath=modules --modulepath=vendor/modules manifests/site.pp
+puppet apply --hiera_config=hiera.yaml --modulepath=modules:vendor/modules manifests/site.pp
