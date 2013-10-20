19aa7660d9f6705b320a3573c1e4ad35bd8093ac
bob walker <bob@randomness.org.uk>
2013-10-20 14:45:16 -0400

    make nodes create sudo::conf resources

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,4 +3,6 @@ node default {
   hiera_include('classes')
   $accounts = hiera('accounts')
   create_resources('account', $accounts)
+  $sudo_conf = hiera('sudo_conf')
+  create_resources('sudo::conf', $sudo_conf)
 }
