3fb0623dc0720b7f226a6fa6a89cb26131a97a4c
bob walker <bob@randomness.org.uk>
2013-10-20 04:35:04 -0400

    Make sure modules are installed

--- apply.sh
+++ apply.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# make sure modules are installed.
+bundle exec librarian-puppet install
+
 sudo puppet apply --hiera_config=hiera.yaml --modulepath=modules:vendor/modules manifests/site.pp
