3f02b7d73f90fe3c10586f05204fd223ed92b666
bob walker <bob@randomness.org.uk>
2013-10-20 04:36:38 -0400

    run rake tasks

--- apply.sh
+++ apply.sh
@@ -3,4 +3,7 @@
 # make sure modules are installed.
 bundle exec librarian-puppet install
 
+# run rake tasks
+bundle exec rake
+
 sudo puppet apply --hiera_config=hiera.yaml --modulepath=modules:vendor/modules manifests/site.pp
