f6b2ffe0786fe70d6cb4ef05fe51000c9a04158d
bob walker <bob@randomness.org.uk>
2013-10-20 10:49:50 +0100

    Apply puppet after you have uploaded repo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# script to bootstrap a machine such that puppet can be applied.
+# script to bootstrap a machine
 
 #rsync the contents of randomness-puppet to a remote machine
 rsync -avzPh --stats --exclude-from=.rsync_exclude   ../randomness-puppet root@$1:
+
+# apply puppet
+ssh root@$1 "cd randomness-puppet; ./apply.sh"
