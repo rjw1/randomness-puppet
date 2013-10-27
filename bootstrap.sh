aaffdad9437d454d954ba7d99a1832b6dab08cea
bob walker <bob@randomness.org.uk>
2013-10-27 09:33:51 +0000

    dont assume root since it wont work after the first run.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 # script to bootstrap a machine
 
 #rsync the contents of randomness-puppet to a remote machine
-rsync -avzPh --stats --exclude-from=.rsync_exclude   ../randomness-puppet root@$1:
+rsync -avzPh --stats --exclude-from=.rsync_exclude   ../randomness-puppet $1:
 
 # apply puppet
-ssh root@$1 "cd randomness-puppet; ./apply.sh"
+ssh $1 "cd randomness-puppet; ./apply.sh"
