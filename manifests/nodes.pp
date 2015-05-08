c16434a4f2cc8880256ae415ae209263eb43df8d
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2015-05-08 17:46:27 +0100

    changes ive been playing with at work

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -5,6 +5,8 @@ node default {
   create_resources('account', $accounts)
   $sudo_conf = hiera('sudo_conf')
   create_resources('sudo::conf', $sudo_conf)
-  $ufw_allow = hiera('ufw_allow')
+  $ufw_allow = hiera_hash('ufw_allow')
   create_resources('ufw::allow', $ufw_allow)
+  $base_packages = hiera_array('base_packages')
+  ensure_packages($base_packages)
 }
