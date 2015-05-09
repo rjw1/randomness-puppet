8b2893c2da974683a656b3b5e0a03d22170117cd
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2015-05-09 08:56:04 +0100

    Make ufw stuff a hiera_hash
    So that machine specific rules can be put in the machines hiera

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -5,6 +5,6 @@ node default {
   create_resources('account', $accounts)
   $sudo_conf = hiera('sudo_conf')
   create_resources('sudo::conf', $sudo_conf)
-  $ufw_allow = hiera('ufw_allow')
+  $ufw_allow = hiera_hash('ufw_allow')
   create_resources('ufw::allow', $ufw_allow)
 }
