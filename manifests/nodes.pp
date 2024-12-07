73674e442f3aea3babae2f03a6c7c941297cd586
bob walker <bob@randomness.org.uk>
2024-12-07 16:07:49 +0000

    remove things that no longer work
    and add stdlib:: to ensure_packages to remove a deprecation warning.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,24 +1,10 @@
 # Use hiera as a lightweight ENC.
 node default {
   lookup('classes', Array[String], 'unique').include
-  $accounts = lookup( { 'name'  => 'accounts',
-          'merge' => {
-            'strategy'        => 'deep',
-          },
-})
-  create_resources('account', $accounts)
-  $sudo_conf = lookup('sudo_conf')
-  create_resources('sudo::conf', $sudo_conf)
-  $ufw_allow = lookup( { 'name' => 'ufw_allow',
-          'merge' => {
-            'strategy'        => 'deep',
-          },
-})
-  create_resources('ufw::allow', $ufw_allow)
   $base_packages = lookup( { 'name' => 'base_packages',
           'merge' => {
             'strategy'        => 'deep',
           },
 })
-  ensure_packages($base_packages)
+  stdlib::ensure_packages($base_packages)
 }
